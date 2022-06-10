4e78cd1d488e96ce415a5bf22197e17ba216b201
Viktor Nagy <viktor.nagy@01people.com>
2022-06-10 09:18:15 +0200

    feat: demo of nested relation fetching

--- pages/api/games.ts
+++ pages/api/games.ts
@@ -1,12 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-  import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const prisma = new PrismaClient()
-  const games = await prisma.game.findMany()
-  res.status(200).json(games)
+  const prisma = new PrismaClient();
+  const games = await prisma.game.findMany({
+    include: {
+      progresses: {
+        select: {
+          progress: true,
+          player: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  res.status(200).json(games);
 }

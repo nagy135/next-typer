06d2366518d154431b6f560057e9cfc90557b271
Viktor Nagy <viktor.nagy@01people.com>
2022-07-06 12:48:48 +0200

    feat: progress bar + pretty WPM badge

--- components/game-body.tsx
+++ components/game-body.tsx
@@ -1,10 +1,12 @@
 import { Game } from "@prisma/client";
 import { useGlobalContext } from "./contexts/global";
 import { XCircleIcon } from "@heroicons/react/solid";
-import { useState } from "react";
+import { CSSProperties, useState } from "react";
 import { CPM_TO_WPM } from "@constants/common";
 
 const GameBody: React.FC<{ game: Game }> = ({ game }) => {
+  const global = useGlobalContext();
+
   const [remaining, setRemaining] = useState(game.text);
   const [writtenCount, setWrittenCount] = useState(0);
   const [wpm, setWpm] = useState(0);
@@ -12,7 +14,6 @@ const GameBody: React.FC<{ game: Game }> = ({ game }) => {
 
   const handleTyping = (e: string) => {
     if (game.text[writtenCount] === e) {
-
       if (!start) setStart(Date.now());
       else {
         const elapsedSec = (Date.now() - start) / 1000;
@@ -24,10 +25,20 @@ const GameBody: React.FC<{ game: Game }> = ({ game }) => {
     }
   };
 
-  const global = useGlobalContext();
+  const progress = Math.round((writtenCount / game.text.length) * 100);
+  const progressStr = (): string => `${progress}%`;
+
   return global.playing ? (
     <div className="mt-2 bg-base-200 flex relative">
-      <div className="absolute left-0 top-0 m-2">{wpm}</div>
+      <div className="absolute left-0 top-0 m-2 flex flex-col">
+        <div className="badge p-4 mb-2">WPM: {wpm}</div>
+        <div
+          className="radial-progress"
+          style={{ "--value": progress } as CSSProperties}
+        >
+          {progressStr()}
+        </div>
+      </div>
       <button
         className="absolute right-0 top-0 m-2"
         onClick={() => global.setPlaying(false)}
@@ -35,8 +46,9 @@ const GameBody: React.FC<{ game: Game }> = ({ game }) => {
         <XCircleIcon className="h-10 w-10 text-red-500" />
       </button>
       <textarea
+        autoFocus
         className="textarea textarea-bordered text-center mx-auto w-1/2 my-2 h-96 hover:drop-shadow-lg"
-        onChange={() => { }}
+        onChange={() => {}}
         onKeyPress={(e) => handleTyping(e.key)}
         value={remaining}
       />
